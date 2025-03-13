edab4c22819cbf5dada9eab35db3a0b2b7bc086b
Prashant Maurya <mauryaprashant1510@gmail.com>
2025-03-13 22:04:35 +0530

    adding loader to login form

--- src/components/LoginPopup/LoginPopup.jsx
+++ src/components/LoginPopup/LoginPopup.jsx
@@ -13,23 +13,30 @@ const LoginPopup = ({setShowLogin}) => {
         email:"",
         password:""
     })
-
+    const[loading, setLoading] = useState(false)
     const{url, setToken} = useContext(StoreContext)
     const onlogin = async (event)=>{
         event.preventDefault();
+        setLoading(true)
         let newUrl = url;
         if (currentState == "Login") {
             newUrl+= '/api/user/login'
         }else{
             newUrl+= '/api/user/register'
         }
-        const response = await axios.post(newUrl,data)
-        if(response.data.success){
-            setToken(response.data.token)
-            localStorage.setItem("token",response.data.token)
-            setShowLogin(false)
-        }else{
-            toast.error(response.data.message)
+        try {
+            const response = await axios.post(newUrl,data)
+            if(response.data.success){
+                setToken(response.data.token)
+                localStorage.setItem("token",response.data.token)
+                setShowLogin(false)
+            }else{
+                toast.error(response.data.message)
+            }
+        } catch (error) {
+            toast.error("Something went wrong. Please try again.")
+        } finally {
+            setLoading(false)
         }
     }
     const onChangeHandler = (event) =>{
@@ -51,7 +58,9 @@ const LoginPopup = ({setShowLogin}) => {
                 <input name='email' onChange={onChangeHandler} value={data.email}  type="email" placeholder='Your Email' required />
                 <input name='password' onChange={onChangeHandler} value={data.password} type="password" placeholder='Password' required />
             </div>
-            <button type='submit'>{currentState === "Sign Up"? "Create Account" : "Login"}</button>
+            <button type='submit' disabled={loading}>
+                    {loading ? 'Processing...' : currentState === "Sign Up" ? "Create Account" : "Login"}
+            </button>
             <div className="login-popup-condition">
                 <input type="checkbox" required />
                 <p>I agree to the term of use and privacy policy</p>
