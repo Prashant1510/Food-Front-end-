03c3c0ccfb469cda8a5d094cb0c5fc17b821849a
Prashant Maurya <mauryaprashant1510@gmail.com>
2025-03-14 03:22:45 +0530

    adding loader for fetching data

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext,useEffect,useState } from "react";
 import "./FoodDisplay.css";
 import { StoreContext } from "../../context/StoreContext.jsx";
 import FoodItem from "../FoodItem/FoodItem.jsx";
@@ -6,27 +6,36 @@ import PropTypes from "prop-types";
 
 const FoodDisplay = ({ category }) => {
   const { food_list } = useContext(StoreContext);
+  const [loading, setLoading] = useState(true);
   
-  
+  useEffect(() => {
+    if (food_list.length > 0) {
+      setLoading(false);
+    }
+  }, [food_list]);
   return (
     <div className="food-display" id="food-display">
       <h2>Top dishes near you</h2>
-      <div className="food-display-list">
-        {food_list.map((item, index) => {
-          if (category === "All" || category === item.category) {
-            return (
-              <FoodItem
-                key={index}
-                id={item._id}
-                name={item.name}
-                description={item.description}
-                image={item.image}
-                price={item.price}
-              />
-            );
-          }
-        })}
-      </div>
+      {loading ? (
+        <p className="fetching-loader">🍔 Just a moment! We’re setting the table...</p>
+      ) : (
+        <div className="food-display-list">
+          {food_list.map((item, index) => {
+            if (category === "All" || category === item.category) {
+              return (
+                <FoodItem
+                  key={index}
+                  id={item._id}
+                  name={item.name}
+                  description={item.description}
+                  image={item.image}
+                  price={item.price}
+                />
+              );
+            }
+          })}
+        </div>
+      )}
     </div>
   );
 };
